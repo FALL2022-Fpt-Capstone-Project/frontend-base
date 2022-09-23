54d24e97982fb4b2d71af8bea6fc753d3b841834
HaiPhuong2000 <nguyenhaiphuong2k@gmail.com>
2022-09-23 13:28:41 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { Button } from "antd";
 import "antd/dist/antd.min.css";
 import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <Button type="primary">Primary Button</Button>
-      <Button>Default Button</Button>
-      <Button type="dashed">Dashed Button</Button>
-      <br />
-      <Button type="text">Text Button</Button>
-      <Button type="link">Link Button</Button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
