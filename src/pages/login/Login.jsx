e79b44de48c437a11ca26378b89015a4c252b871
HaiPhuong2000 <nguyenhaiphuong2k@gmail.com>
2022-09-25 23:04:56 +0700

    test api login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Checkbox, Form, Input } from "antd";
 import "antd/dist/antd.min.css";
 import "./login.scss";
 
 const Login = () => {
+  const [user_name, setUser_name] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    let result = await fetch("https://rms-staging-env.herokuapp.com/api/auth/signin", {
+      method: "post",
+      body: JSON.stringify({ user_name, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+  };
+  console.log(user_name);
+
   return (
     <div className="login-page">
       <div className="login-box">
@@ -13,14 +28,14 @@ const Login = () => {
             alt="Login"
           />
         </div>
-        <Form name="login-form" initialValues={{ remember: true }}>
+        <Form name="login-form" initialValues={{ remember: true }} onFinish={handleLogin}>
           <p className="form-title">Đăng nhập</p>
           <Form.Item name="phone" rules={[{ required: true, message: "Vui lòng nhập số điện thoại!" }]}>
-            <Input placeholder="Số điện thoại" />
+            <Input placeholder="Số điện thoại" onChange={(e) => setUser_name(e.target.value)} value={user_name} />
           </Form.Item>
 
           <Form.Item name="password" rules={[{ required: true, message: "Vui lòng nhập mật khẩu!" }]}>
-            <Input.Password placeholder="Mật khẩu" />
+            <Input.Password placeholder="Mật khẩu" onChange={(e) => setPassword(e.target.value)} value={password} />
           </Form.Item>
 
           <Form.Item>
