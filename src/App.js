34dbfc48c892da68446b1b281fc0afc5eaeaf2b9
HaiPhuong2000 <nguyenhaiphuong2k@gmail.com>
2022-11-20 22:07:17 +0700

    create branh invoice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.scss";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 import Building from "./pages/building/Building";
